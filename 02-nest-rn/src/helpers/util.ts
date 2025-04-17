8888263490a83fa0022a36bf88c45143f649d7e0
nam.tran3 <nam.tran3@ekotek.vn>
2025-04-17 14:28:41 +0700

    Remove submodule 01-next-rn

--- 02-nest-rn/src/helpers/util.ts
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt')
-const saltRounds = 10
-
-export const hashPasswordHelper = async (password: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(saltRounds)
-  return bcrypt.hash(password, salt)
-}
